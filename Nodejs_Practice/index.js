041ed8b438992e4deffd9a56c41f5522ebd1b2e4
h95fit <h95fit@gmail.com>
2022-12-11 18:52:46 +0900

    Nodejs - express 모듈 기초

--- Nodejs_Practice/index.js
+++ Nodejs_Practice/index.js
@@ -12,19 +12,67 @@ npm (node package manager)
 
 
 Express 모듈
-- node.js를 이용해서 웹 프레임워크를 만듦
+- node.js를 이용해서 웹 프레임워크를 모듈
+
+주요 포트 리스트
+FTP : 20, 21 (TCP)
+SSH : 22 (TCP)
+Telnet : 23 (TCP)
+SMTP : 25 (TCP)
+DNS : 53 (TCP/UDP)
+DHCP : 67 (UDP)
+HTTP : 80 (TCP)
+HTTPS : 443 (TCP)
 
 */
 
 // npm 실습 - figlet 활용하기
-var figlet = require('figlet');
+// var figlet = require('figlet');
 
-figlet('Huns Project', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+// figlet('Huns Project', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
 
+// express 웹 동작 테스트
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {       // app.[HTTP 메소드]('[라우팅]', [콜백함수])    + 콜백함수 형태 : () => {} /콜백함수란 HTTP 메소드 함수가 끝난 뒤에 실행할 함수를 의미!
+    res.send('Hello World!')
+})
+
+app.get('/dog', (req, res) => {
+    res.send('<h1>강아지<h1>')        // HTML 코드도 요청으로 보낼 수 있다!
+})
+
+app.get('/cat', (req, res) => {
+    res.json({'sound' : '야옹'})    // json 형태로 보냄(키:값)
+})
+
+// 변수 (params)를 이용한 요청-응답
+app.get('/user/:id', (req, res) => {
+    const q = req.params  
+    console.log(q.id)
+
+    res.send(q.id)
+})
+
+
+// query를 이용한 요청-응답
+app.get("/user/:id", (req, res) => {
+    const p = req.query
+    console.log(p)
+
+    res.send(p.id)
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
