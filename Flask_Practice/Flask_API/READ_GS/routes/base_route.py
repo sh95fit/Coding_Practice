69f12e28bd8a4eb48085d8363cbdf11f98d3c6fc
sh95fit <h95fit@gmail.com>
2023-05-03 17:22:01 +0900

    Flask - jinja template 활용(특정 값을 입력 받아 조회용 API를 호출하는 소스 작성)

--- Flask_Practice/Flask_API/READ_GS/routes/base_route.py
+++ Flask_Practice/Flask_API/READ_GS/routes/base_route.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
+import requests
 
 
 NAME = 'BASE'
@@ -10,3 +11,17 @@ bp = Blueprint(NAME, __name__)
 def index() :
   return 'READ GSREMS'
 
+@bp.route('/rawdata', methods=['GET'])
+def rawdata() :
+  rtu_id = request.args.get('rtu_id')
+  date_id = request.args.get('date_id')
+
+  # API 호출
+  url = f"http://127.0.0.1:5000/api/pw_raw/{rtu_id}/{date_id}"
+  response = requests.get(url)
+  if response.ok :
+    data = response.json()
+  else :
+    data = None
+  # data = requests.get(url)
+  return render_template('index.html', data=data)
