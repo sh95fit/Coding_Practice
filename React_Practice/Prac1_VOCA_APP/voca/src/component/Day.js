9dd8c4a63f8eb457944c537389c7d6d0597aa2ec
h95fit <h95fit@gmail.com>
2023-12-20 23:08:07 +0900

    json-server db에서 useEffect, fetch로 데이터 불러오기

--- React_Practice/Prac1_VOCA_APP/voca/src/component/Day.js
+++ React_Practice/Prac1_VOCA_APP/voca/src/component/Day.js
@@ -1,27 +1,34 @@
 import dummy from "../db/data.json"
 import { useParams } from "react-router-dom";
+import Word from "./Word";
+import { useState, useEffect } from "react"
 
 export default function Day(){
     const day = useParams().day;
-    const wordList = dummy.words.filter(word => word.day === Number(day));
+    // const wordList = dummy.words.filter(word => word.day === Number(day));
     // console.log(wordList)
+    const [words, setWords] = useState([]);
 
-    return <>
-    <h2>Day {day}</h2>
-    <table>
-        <tbody>
-            {wordList.map(word => (
-            <tr key={word.id}>
-                <td>
-                    {word.eng}
-                </td>
-                <td>
-                    {word.kor}
-                </td>
-            </tr>
-            ))}
-        </tbody>
-    </table>
-    
-    </>
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+        })
+    }, [day]);
+
+    return (
+        <>
+            <h2>Day {day}</h2>
+            <table>
+                <tbody>
+                    {words.map(word => (
+                        <Word word={word} key={word.id}/>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
 }
--- React_Practice/Prac1_VOCA_APP/voca/src/component/DayList.js
+++ React_Practice/Prac1_VOCA_APP/voca/src/component/DayList.js
@@ -1,15 +1,25 @@
-import dummy from "../db/data.json";
 import {Link} from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function DayList() {
-    console.log(dummy);
+    const [days, setDays] = useState([]);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/days')
+        .then(res => {return res.json()})
+        .then(data => setDays(data))
+    }, [])
+
     return (
-        <ul className="list_day">
-            {dummy.days.map(day => (
-                <li key={day.id}>
-                    <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
